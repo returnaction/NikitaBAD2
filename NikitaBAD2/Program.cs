d73b2d143ceb95d968bfa3e4f1e1d3bbe01d0236
returnaction <obergannv@gmail.com>
2024-03-13 22:44:31 -0400

    Okay It works

--- NikitaBAD2/Program.cs
+++ NikitaBAD2/Program.cs
@@ -10,7 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
@@ -32,7 +31,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -50,3 +48,8 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+
+
+// bambo@gmail.com sldk@#SDF122
+// kastet@gmail.com sldk@#SDF122
+// admin@gmail.com sldk@#SDF122
