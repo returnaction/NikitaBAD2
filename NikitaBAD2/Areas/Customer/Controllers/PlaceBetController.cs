3a4e3bbed5a55222f599230a05d5d4d67f48085f
returnaction <obergannv@gmail.com>
2024-03-16 10:49:09 -0400

    PlaceBet is Done but except 10 because it's way to easy to implement

--- NikitaBAD2/Areas/Customer/Controllers/PlaceBetController.cs
+++ NikitaBAD2/Areas/Customer/Controllers/PlaceBetController.cs
@@ -19,6 +19,17 @@ namespace NikitaBAD2.Areas.Customer.Controllers
 
             switch (rolledNumber)
             {
+                case 5:
+                case 9:
+                    if (userAnswer == CaluclateCorrectAnswerFor5or9(bet))
+                        return RedirectToAction(nameof(Play));
+                    else
+                    {
+                        placeBet.RolledNumber = rolledNumber;
+                        placeBet.Bet = bet;
+                        placeBet.ErrorMessage = "Wrong Payout!";
+                    }
+                    break;
                 case 6:
                 case 8:
                     if (userAnswer == CaluclateCorrectAnswerFor6or8(bet))
@@ -42,6 +53,10 @@ namespace NikitaBAD2.Areas.Customer.Controllers
 
             switch (placeBet.RolledNumber)
             {
+                case 5:
+                case 9:
+                    placeBet.Bet = GenerateRandomBetFor4or10or5or9();
+                    break;
                 case 6:
                 case 8:
                     placeBet.Bet = GenerateRandomBetFor6or8();
@@ -63,6 +78,29 @@ namespace NikitaBAD2.Areas.Customer.Controllers
             return bet + (bet / 6);
         }
 
+        private int GenerateRandomBetFor4or10or5or9()
+        {
+            Random random = new();
+            int bet = random.Next(1, 101) * 5;
+            return bet;
+        }
+
+        private int CaluclateCorrectAnswerFor5or9(int bet)
+        {
+            return bet / 5 * 7;
+        }
+
+        //TODO: did not implented yet
+        private int CaluclateCorrectAnswerFor4or10(int bet)
+        {
+            if (bet <= 15)
+                return bet / 5 * 9;
+            else
+                return bet * bet;
+        }
+
+
+        //TODO: Add 4 and 10
         private int RollDice()
         {
             Random random = new();
