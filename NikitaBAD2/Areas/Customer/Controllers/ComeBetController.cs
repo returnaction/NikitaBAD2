b0a98c3a49342a773f91976df8b06d88fdfb59f1
returnaction <obergannv@gmail.com>
2024-03-15 22:20:11 -0400

    Add Class ComeBet

---
+++ NikitaBAD2/Areas/Customer/Controllers/ComeBetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NikitaBAD2.Areas.Customer.Controllers
+{
+    public class ComeBetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NikitaBAD2/Models/PropBets/ComeBet.cs
@@ -0,0 +1,12 @@
+﻿namespace NikitaBAD2.Models.PropBets
+{
+    public class ComeBet
+    {
+        public int FlatBet { get; set; }
+        public int Odds { get; set; }
+        public int RolledNumber { get; set; }
+        public string ErrorMessage { get; set; }
+        public int CorrectAnswer { get; set; }
+
+    }
+}
