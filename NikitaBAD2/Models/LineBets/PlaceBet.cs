7a7a06d2ac2b761b60999a13e0a2513f4442a019
returnaction <obergannv@gmail.com>
2024-03-16 00:47:32 -0400

    Add PlaceBet class

---
+++ NikitaBAD2/Models/LineBets/PlaceBet.cs
@@ -0,0 +1,13 @@
+﻿namespace NikitaBAD2.Models.LineBets
+{
+    public class PlaceBet
+    {
+        public int Bet { get; set; }
+        public int? Cap { get; set; } // might not going to use it let's see
+        public int RolledNumber { get; set; }
+        public string ErrorMessage { get; set; } = null!;
+        public int CorrectAnswer { get; set; }
+
+
+    }
+}
